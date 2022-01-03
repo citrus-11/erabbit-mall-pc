e72e05240879423275b968dade781e47507ca2f2
CITRUS11 <citrus11@foxmail.com>
2022-01-03 14:24:49 +0800

    add topnav

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 重置样式库
+import 'normalize.css'
+// 项目公用重置样式
+import '@/assets/styles/common.less'
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,8 +1,20 @@
 <template>
-  <nav>top</nav>
+  <AppTopnav />
   <header>header</header>
   <main>
     <RouterView />
   </main>
   <footer>footer</footer>
+  <button @click="login">login</button>
+  <button @click="exit">exit</button>
 </template>
+
+<script>
+import AppTopnav from '@/components/app-topnav.vue'
+export default {
+  name: 'Layout',
+  components: {
+    AppTopnav
+  }
+}
+</script>
