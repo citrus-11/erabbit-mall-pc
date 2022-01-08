a8d1713a7f968ad7b45794acb59a0cf3fac09110
CITRUS11 <citrus11@foxmail.com>
2022-01-08 20:21:41 +0800

    code style change

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore } from 'vuex'
-import createPersistedstate from 'vuex-persistedstate'
+import { createStore } from 'vuex';
+import createPersistedstate from 'vuex-persistedstate';
 
 // 购物车/用户持久化模块
-import cart from './modules/cart'
-import user from './modules/user'
+import cart from './modules/cart';
+import user from './modules/user';
 
 // 分类模块不需要缓存
-import category from './modules/category'
+import category from './modules/category';
 
 export default createStore({
   modules: { cart, user, category },
@@ -16,4 +16,4 @@ export default createStore({
       paths: ['cart', 'user']
     })
   ]
-})
+});
